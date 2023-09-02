b0df92b5cb2aefb32746e63a5853a82e19386aaf
Abdullahahmed11 <abdullah77zzx@gmail.com>
2023-09-02 15:34:42 +0300

    finalHome

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 
 function Footer() {
+
+
     return (
         <>
-            <div className=" p-5 bottom-0 absolute w-full dark:text-white border-t-4 ">
+            <div className=" p-5  w-full dark:text-white border-t-4 ">
                 <div className="flex items-center justify-center ">
                     <h1 className="">© 2023 Outstock - Footer</h1>
                 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        accent: "blue['400']",
+        primary: 'white',
+        secondary: '#b6bbb6',
+        background: '#1a1a1a',
+        'main-red': "red['400']",
+        'border-primary': "neutral['700']",
+        'border-secondary': "neutral['400']"
+      }
     },
   },
   plugins: [],
